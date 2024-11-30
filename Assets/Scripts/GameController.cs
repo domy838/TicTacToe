04142273b7d0597952d8e44850d0e40831a246ac
domy838 <domy@tlu.ee>
2024-11-30 11:43:22 +0200

    Adding necessary functions

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,4 +7,27 @@ using TMPro;
 public class GameController : MonoBehaviour
 {
     public List<TMP_Text> buttonList;
+
+    void Start () 
+    {
+         SetGameControllerReferenceOnButtons();
+    }
+
+    void SetGameControllerReferenceOnButtons () 
+    {
+        for (int i=0; i<buttonList.Count; i++)
+        {
+            buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
+        }
+    }
+
+    public string GetPlayerSide () 
+    {
+         return "?";
+    }
+
+    public void EndTurn () 
+    { 
+        Debug.Log("EndTurn is not implemented!"); 
+    }
 }
