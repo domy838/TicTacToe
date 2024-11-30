391e996fba16cc78cc9ba136af894ae12d6223b0
domy838 <domy@tlu.ee>
2024-11-30 11:47:28 +0200

    First end condition

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,10 +7,12 @@ using TMPro;
 public class GameController : MonoBehaviour
 {
     public List<TMP_Text> buttonList;
+    private string playerSide;
 
     void Start () 
     {
-         SetGameControllerReferenceOnButtons();
+        SetGameControllerReferenceOnButtons();
+        playerSide = "X";
     }
 
     void SetGameControllerReferenceOnButtons () 
@@ -23,11 +25,22 @@ public class GameController : MonoBehaviour
 
     public string GetPlayerSide () 
     {
-         return "?";
+         return playerSide;
     }
 
     public void EndTurn () 
     { 
-        Debug.Log("EndTurn is not implemented!"); 
+        if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide) 
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver () 
+    {
+        for (int i=0; i<buttonList.Count; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = false;
+        }
     }
 }
