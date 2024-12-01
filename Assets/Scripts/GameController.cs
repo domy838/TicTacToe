c95969c2ea26c6b192aab805e881e83714abfeb0
domy838 <domy@tlu.ee>
2024-12-01 10:30:04 +0200

    Restart button and panels for playing side

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+[System.Serializable]
+public class Player
+{
+    public Image panel;
+    public TMP_Text text;
+}
+
+[System.Serializable]
+public class PlayerColor
+{
+    public Color panelColor;
+    public Color textColor;
+}
+
 public class GameController : MonoBehaviour
 {
     public List<TMP_Text> buttonList;
@@ -11,6 +25,12 @@ public class GameController : MonoBehaviour
     public GameObject gameOverPanel;
     public TMP_Text gameOverText;
     private int moveCount;
+    public GameObject restartButton;
+
+    public Player playerX; 
+    public Player playerO; 
+    public PlayerColor activePlayerColor; 
+    public PlayerColor inactivePlayerColor;
 
     void Start () 
     {
@@ -18,6 +38,8 @@ public class GameController : MonoBehaviour
         playerSide = "X";
         gameOverPanel.SetActive(false);
         moveCount = 0;
+        restartButton.SetActive(false);
+        SetPlayerColors(playerX, playerO);
     }
 
     void SetGameControllerReferenceOnButtons() 
@@ -35,41 +57,62 @@ public class GameController : MonoBehaviour
 
     public void EndTurn() 
     { 
-        for (int i=0; i<=8; i=i+3)
-        {
-            if(buttonList[i].text == playerSide && buttonList[i+1].text == playerSide && buttonList[i+2].text == playerSide)
-            {
-                GameOver();
-            }
-        }
-        for (int i=0; i<3; i++)
-        {
-            if(buttonList[i].text == playerSide && buttonList[i+3].text == playerSide && buttonList[i+6].text == playerSide)
-            {
-                GameOver();
-            }
-        }
-        if (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide) 
-        {
-            GameOver();
-        }
-        if (buttonList[6].text == playerSide && buttonList[4].text == playerSide && buttonList[2].text == playerSide) 
-        {
-            GameOver();
-        }
         moveCount++;
-        if (moveCount >= 9) 
-        { 
-            SetGameOverText("It's a draw!");
+        if (buttonList [0].text == playerSide && buttonList [1].text == playerSide && buttonList [2].text == playerSide)
+        {
+           GameOver(playerSide);
+        } 
+        else if (buttonList [3].text == playerSide && buttonList [4].text == playerSide && buttonList [5].text == playerSide)
+        {
+           GameOver(playerSide);
+        } 
+        else if (buttonList [6].text == playerSide && buttonList [7].text == playerSide && buttonList [8].text == playerSide)
+        {
+           GameOver(playerSide);
+        } 
+        else if (buttonList [0].text == playerSide && buttonList [3].text == playerSide && buttonList [6].text == playerSide)
+        {
+           GameOver(playerSide);
+        } 
+        else if (buttonList [1].text == playerSide && buttonList [4].text == playerSide && buttonList [7].text == playerSide)
+        {
+           GameOver(playerSide);
+        } 
+        else if (buttonList [2].text == playerSide && buttonList [5].text == playerSide && buttonList [8].text == playerSide)
+        {
+           GameOver(playerSide);
+        } 
+        else if (buttonList [0].text == playerSide && buttonList [4].text == playerSide && buttonList [8].text == playerSide)
+        {
+           GameOver(playerSide);
         }
-        ChangeSides();
+	    else if (buttonList [2].text == playerSide && buttonList [4].text == playerSide && buttonList [6].text == playerSide)
+        {
+           GameOver(playerSide);
+        } 
+       else if (moveCount >= 9)
+        {
+           GameOver("draw");
+        } 
+       else
+        {
+           ChangeSides();
+        }
     }
 
-    void GameOver() 
+    void GameOver(string winningPlayer) 
     {
+        if (winningPlayer == "draw") 
+        { 
+            SetGameOverText("It's a Draw!"); 
+        } 
+        else 
+        {
+            SetGameOverText(winningPlayer + " Wins!"); 
+        }
         SetBoardInteractable(false);
-        SetGameOverText(playerSide + " Wins!");
         moveCount --;
+        restartButton.SetActive(true);
     }
 
     void ChangeSides() 
@@ -77,10 +120,12 @@ public class GameController : MonoBehaviour
         if(playerSide == "X")
         {
             playerSide = "O";
+            SetPlayerColors(playerO, playerX);
         }
         else
         {
             playerSide = "X";
+            SetPlayerColors(playerX, playerO);
         }
     }
 
@@ -100,7 +145,10 @@ public class GameController : MonoBehaviour
         {
             buttonList[i].text = ""; 
         }
+        restartButton.SetActive(false);
+        SetPlayerColors(playerX, playerO);
     }
+
     void SetBoardInteractable (bool toggle) 
     {
         for (int i = 0; i < buttonList.Count; i++) 
@@ -108,4 +156,12 @@ public class GameController : MonoBehaviour
             buttonList[i].GetComponentInParent<Button>().interactable = toggle; 
         }
     }
+
+    void SetPlayerColors (Player newPlayer, Player oldPlayer)
+    {
+        newPlayer.panel.color = activePlayerColor.panelColor; 
+        newPlayer.text.color = activePlayerColor.textColor; 
+        oldPlayer.panel.color = inactivePlayerColor.panelColor; 
+        oldPlayer.text.color = inactivePlayerColor.textColor;
+    }  
 }
